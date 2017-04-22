28befec62c74b9d953519e5cd629e0a073151c34
Kevin Bowersox <kmb385@gmail.com>
2017-04-22 09:50:25 -0400

    adding

---
+++ src/main/java/com/oreilly/jenkins/HelloWorldMaven.java
@@ -0,0 +1,10 @@
+package com.oreilly.jenkins;
+
+public class HelloWorldMaven {
+
+	public static void main(String[] args) {
+		MessageSource msgSource = new MessageSource();
+		msgSource.getMessage();
+	}
+	
+}
